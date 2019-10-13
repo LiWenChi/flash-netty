fade8aaeecc4ae8be931171924c3e57e5f75b5e1
李文驰 <liwenchi@songshuai.com>
2019-10-13 20:08:10 +0800

    添加相应的注解

--- src/main/java/the/flash/client/ClientHandler.java
+++ src/main/java/the/flash/client/ClientHandler.java
@@ -12,11 +12,18 @@ import java.util.Date;
 import java.util.UUID;
 
 /**
+ * 登录逻辑中客户端的处理逻辑
  * @author chao.yu
  * chao.yu@dianping.com
  * @date 2018/08/04 06:23.
  */
 public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当客户端与服务器建立简介以后就会回调该方法
+     * 客户端连接上服务器后会理解登录
+     * @param ctx
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) {
         System.out.println(new Date() + ": 客户端开始登录");
@@ -28,6 +35,8 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
         loginRequestPacket.setPassword("pwd");
 
         // 编码
+        //  通过单例对象进行编码
+        //  `ctx.alloc()` 获取的就是与当前连接相关的 `ByteBuf` 分配器，建议这样来使用
         ByteBuf buffer = PacketCodeC.INSTANCE.encode(ctx.alloc(), loginRequestPacket);
 
         // 写数据
@@ -35,10 +44,16 @@ public class ClientHandler extends ChannelInboundHandlerAdapter {
     }
 
 
+    /**
+     * 客户端接收到服务端的请求相应后调用该方法
+     * @param ctx
+     * @param msg 服务端发送过来的请求相应数据
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf byteBuf = (ByteBuf) msg;
 
+        //解码
         Packet packet = PacketCodeC.INSTANCE.decode(byteBuf);
 
         if (packet instanceof LoginResponsePacket) {
--- src/main/java/the/flash/server/ServerHandler.java
+++ src/main/java/the/flash/server/ServerHandler.java
@@ -11,19 +11,27 @@ import the.flash.protocol.response.LoginResponsePacket;
 import java.util.Date;
 
 /**
+ * 登录逻辑中服务端的处理逻辑
  * @author chao.yu
  * chao.yu@dianping.com
  * @date 2018/08/04 06:21.
  */
 public class ServerHandler extends ChannelInboundHandlerAdapter {
 
+    /**
+     * Netty接收到请求后回调该方法
+     * @param ctx
+     * @param msg 客户端发送过来的二进制数据
+     */
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         System.out.println(new Date() + ": 客户端开始登录……");
         ByteBuf requestByteBuf = (ByteBuf) msg;
 
+        //解码为Packet对象
         Packet packet = PacketCodeC.INSTANCE.decode(requestByteBuf);
 
+        //判断是否是LoginRequestPacket
         if (packet instanceof LoginRequestPacket) {
             // 登录流程
             LoginRequestPacket loginRequestPacket = (LoginRequestPacket) packet;
@@ -38,9 +46,11 @@ public class ServerHandler extends ChannelInboundHandlerAdapter {
                 loginResponsePacket.setSuccess(false);
                 System.out.println(new Date() + ": 登录失败!");
             }
+
             // 登录响应
+            //  将服务端的相应编码为二进制数据返回给客户端
             ByteBuf responseByteBuf = PacketCodeC.INSTANCE.encode(ctx.alloc(), loginResponsePacket);
-            ctx.channel().writeAndFlush(responseByteBuf);
+            ctx.channel().writeAndFlush(responseByteBuf);//数据写回客户端
         }
     }
 
